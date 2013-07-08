faa8cdf2d1e064d9d6824ad8b565b068b9278c40
swytman <swytman@gmail.com>
2013-07-08 22:30:41 +0400

    2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Swytchat::Application.config.secret_key_base = '7f9ed36a0c38986c62a5ab73039f89d7cb9fff43dbba41360cdf0334ca7613efc7f6f62843cf93ff0f4b349251eecb922d19f238b084dc8c8a27074bb9e7053a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Swytchat::Application.config.session_store :cookie_store, key: '_swytchat_session'
