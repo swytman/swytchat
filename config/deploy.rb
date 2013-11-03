759f4268bab5e9fa9b7d85a2ed0c5fb45c6c2fe3
swytman <swytman@gmail.com>
2013-11-03 19:25:41 +0400

    Symlink to database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,37 @@ server "swyttalk.ru", :app, :web, :db, :primary => true
 set :unicorn_conf, "#{deploy_to}/current/config/unicorn.rb"
 set :unicorn_pid, "#{deploy_to}/current/shared/pids/unicorn.pid"
 
+after 'deploy:update_code', 'deploy:symlink_shared'
 
+def run_remote_rake(rake_cmd)
+  rake_args = ENV['RAKE_ARGS'].to_s.split(',')
+  cmd = "cd #{fetch(:latest_release)} && #{fetch(:rake, "rake")} RAILS_ENV=#{fetch(:rails_env, "production")} #{rake_cmd}"
+  cmd += "['#{rake_args.join("','")}']" unless rake_args.empty?
+  run cmd
+  set :rakefile, nil if exists?(:rakefile)
+end
+ 
+#DEPLOY
+namespace :deploy do
+  desc "Restart Resque Workers"
+  task :restart_workers, :roles => :db do
+    run_remote_rake "resque:restart_workers"
+  end
+ 
+  desc "Restart Resque scheduler"
+  task :restart_scheduler, :roles => :db do
+    run_remote_rake "resque:restart_scheduler"
+  end
+
+  desc "Symlinks"
+  task :symlink_shared do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+
+end
+
+
+# UNICORN
 namespace :unicorn do
   task :restart do
     run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D; fi"
@@ -31,6 +61,7 @@ namespace :unicorn do
   end
 end
 
+#NGINX
 namespace :nginx do
   task :restart do
     sudo "sudo service nginx restart"
@@ -40,26 +71,6 @@ namespace :nginx do
   end
   task :stop do
     sudo "sudo service nginx stop"
-  end
-end
-
-def run_remote_rake(rake_cmd)
-  rake_args = ENV['RAKE_ARGS'].to_s.split(',')
-  cmd = "cd #{fetch(:latest_release)} && #{fetch(:rake, "rake")} RAILS_ENV=#{fetch(:rails_env, "production")} #{rake_cmd}"
-  cmd += "['#{rake_args.join("','")}']" unless rake_args.empty?
-  run cmd
-  set :rakefile, nil if exists?(:rakefile)
-end
- 
-namespace :deploy do
-  desc "Restart Resque Workers"
-  task :restart_workers, :roles => :db do
-    run_remote_rake "resque:restart_workers"
-  end
- 
-  desc "Restart Resque scheduler"
-  task :restart_scheduler, :roles => :db do
-    run_remote_rake "resque:restart_scheduler"
   end
 end
 
