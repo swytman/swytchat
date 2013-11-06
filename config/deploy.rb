e145e5c5921061bb4eeb5036e831b1e005825253
swytman <swytman@gmail.com>
2013-11-06 23:17:49 +0400

    little design change

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,13 +54,14 @@ end
 # UNICORN
 namespace :unicorn do
   task :restart do
-    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D; fi"
+    stop
+    start
   end
   task :start do
     run "cd #{current_path}; bundle exec unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D"
   end
   task :stop do
-    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
+    run "kill -QUIT `ps -ef | grep unicorn | grep -v grep | awk '{print $2}'`"
   end
 end
 
